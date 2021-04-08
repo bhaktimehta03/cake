a2684c507ea6624109aed720905a4b9160b23a3f
shahmilanp86 <shah.milan.p@gmail.com>
2021-04-07 21:39:04 -0500

    update cake list based on category selection

--- src/components/BrowseCake.js
+++ src/components/BrowseCake.js
@@ -1,18 +1,46 @@
 import React from 'react'
 import CategoryList from './CategoryList.js'
 import CakeList from './CakeList.js'
+import reactDom from 'react-dom';
 
 
+class BrowseCake extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { isHidden: 0, selectedCategory: 3, }
+    }
+
+    updateCakeListBasedOnCatergorySeclection(inp) {
+        console.log(" ca13", inp);
+        this.setState({ selectedCategory: inp })
+    }
+
+    render() {
+        //selectedCategory = 2;
+        let display;
+        if (!this.state.isHidden) {
+            display = <CakeList selectedCategory={this.state.selectedCategory} ></CakeList>
 
 
-function BrowseCake() {
-    return (
-        <div>
-            <CategoryList></CategoryList>
-            <CakeList></CakeList>
-        </div>
-    )
+        }
+
+        return (<div>
+            <CategoryList updateCakeListBasedOnCatergorySeclection={this.updateCakeListBasedOnCatergorySeclection.bind(this)}></CategoryList>
+            {display
+            }</div>)
+
+    }
 }
+
+// function BrowseCake() {
+//     return (
+//         <div>
+//             <CategoryList></CategoryList>
+//             <CakeList></CakeList>
+//         </div>
+//     )
+// }
 
 
 export default BrowseCake;
